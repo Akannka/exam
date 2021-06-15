7ac90c6af70333cd15bf7fe0ca40601210641e5b
Anna Kovalyova <akannka@gmail.com>
2021-06-15 10:43:07 +0300

    new comment (for new branch)

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void proccess(const std::string& input) {
     {
         if (isLowerCase(input[i]) && isLowerCase(input[i + 1])) // If both elements are in the range [a..z]
         {
-            if (input[i] < input[i + 1])
+            if (input[i] < input[i + 1]) //if 1 is less than 2
             {
                 result = input.substr(i, 2);
                 std::cout << result.length() << " " << result << std::endl;
