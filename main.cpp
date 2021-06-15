a6055d27199badd24b695526f84645a4bb98dcde
Anna Kovalyova <akannka@gmail.com>
2021-06-15 10:15:55 +0300

    method process()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void proccess(const std::string& input)
+{
+    std::string result = "";
+    int start = 0, end = 0;
+    int i = 0;
+    while (i < input.length()-1) // Until the penultimate element
+    {
+        //smth
+    }
+
 }
